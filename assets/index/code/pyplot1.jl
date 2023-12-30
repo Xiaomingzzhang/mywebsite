ea5caf5a0d384d2a2fe5ea2dd56935879bc1c25f
Xiaomingzzhang <105960901+Xiaomingzzhang@users.noreply.github.com>
2023-12-30 11:08:06 +0000

    Deploying to gh-pages from  @ 1f3aaf7161374eb0e1910e69230a588970c13833 🚀

--- assets/index/code/pyplot1.jl
+++
@@ -1,8 +0,0 @@
-# This file was generated, do not modify it. # hide
-using Plots
-x = range(-2, 2, length=500)
-plot()
-for α in 1:5
-    plot!(x, sinc.(α .* x))
-end
-savefig(joinpath(@OUTPUT, "sinc.svg")) # hide
